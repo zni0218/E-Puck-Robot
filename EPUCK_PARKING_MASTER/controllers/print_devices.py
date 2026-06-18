91ffa30d24d023117177bb07b538a73caa9be155
Zhixu Ni <zni.0218@gmail.com>
2026-06-18 21:44:36 +0100

    Add: inclui  ambiente Python

--- EPUCK_PARKING_MASTER/controllers/print_devices.py
+++
@@ -1,7 +0,0 @@
-"""
-Some useful functions for Webots and for working with the epuck robot (https://cyberbotics.com/doc/guide/epuck?version=R2021a).
-By: Gonçalo Leão
-"""
-from controllers.utils import print_devices
-
-print_devices()
